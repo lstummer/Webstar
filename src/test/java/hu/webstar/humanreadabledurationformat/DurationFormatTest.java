e77d09fbe5e1903510b75a4622adc32f91769a15
lstummer <leslie.stummer@gmail.com>
2021-07-02 07:34:42 +0200

    jUnit has been added to pom.xml.

---
+++ src/test/java/hu/webstar/humanreadabledurationformat/DurationFormatTest.java
@@ -0,0 +1,10 @@
+package hu.webstar.humanreadabledurationformat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DurationFormatTest {
+
+    @org.junit.jupiter.api.Test
+    void formatDuration() {
+    }
+}
