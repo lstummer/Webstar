595832dad7affe431e5d37bbfdfc1b0f79bcbb6d
lstummer <leslie.stummer@gmail.com>
2021-07-02 07:13:43 +0200

    formatDuration method is done.

--- src/main/java/hu/webstar/humanreadabledurationformat/DurationFormat.java
+++ src/main/java/hu/webstar/humanreadabledurationformat/DurationFormat.java
@@ -1,11 +1,20 @@
 package hu.webstar.humanreadabledurationformat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DurationFormat {
 
     private int seconds;
 
     public String formatDuration(int seconds) {
+
+        final String[] TIME_COMPONENTS = {"year", "day", "hour", "minute", "second"};
+        final String[] TIME_COMPONENTS_PLURAL = {"years", "days", "hours", "minutes", "seconds"};
+
         String strResult = null;
+        List<Integer> listComponents = new ArrayList<>();
+        int i;
 
         if (seconds < 0) {
             throw new IllegalArgumentException("Parameter can't be smaller than zero!");
@@ -16,6 +25,32 @@ public class DurationFormat {
         }
 
         this.seconds = seconds;
+
+        listComponents.add(getYear());
+        listComponents.add(getDay());
+        listComponents.add(getHour());
+        listComponents.add(getMinute());
+        listComponents.add(getSecond());
+
+        i = 0;
+        while (i < listComponents.lastIndexOf(0) - 1) {
+            if (listComponents.get(i) > 0) {
+                strResult += listComponents.get(i) + " ";
+                if (listComponents.get(i) > 1) {
+                    strResult += TIME_COMPONENTS_PLURAL[i];
+                } else {
+                    strResult += TIME_COMPONENTS[i];
+                }
+                strResult += ", ";
+            }
+            i++;
+        }
+        strResult += " and " + listComponents.get(i);
+        if (listComponents.get(i) > 1) {
+            strResult += TIME_COMPONENTS_PLURAL[i];
+        } else {
+            strResult += TIME_COMPONENTS[i];
+        }
 
         return strResult;
     }
