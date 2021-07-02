f4359efd3d1cc02f3a129ad09d0d0dc917093385
lstummer <leslie.stummer@gmail.com>
2021-07-02 05:56:59 +0200

    The getters what divide input seconds to years, days, hours, minutes and seconds are done.

--- src/main/java/hu/webstar/humanreadabledurationformat/DurationFormat.java
+++ src/main/java/hu/webstar/humanreadabledurationformat/DurationFormat.java
@@ -2,8 +2,10 @@ package hu.webstar.humanreadabledurationformat;
 
 public class DurationFormat {
 
+    private int seconds;
+
     public String formatDuration(int seconds) {
-        String strResult=null;
+        String strResult = null;
 
         if (seconds < 0) {
             throw new IllegalArgumentException("Parameter can't be smaller than zero!");
@@ -13,6 +15,28 @@ public class DurationFormat {
             return "now";
         }
 
+        this.seconds = seconds;
+
         return strResult;
+    }
+
+    public int getYear() {
+        return seconds / 31536000;
+    }
+
+    public int getDay() {
+        return (seconds % 31536000) / 86400;
+    }
+
+    public int getHour() {
+        return (seconds % 86400) / 3600;
+    }
+
+    public int getMinute() {
+        return (seconds % 3600) / 60;
+    }
+
+    public int getSecond() {
+        return seconds % 60;
     }
 }
